6b031c3c37009983e37255c261d6acb98e645861
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-03-30 23:13:17 +0530

    Added: Frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "carbon-components-svelte/css/g80.css";
+  import "virtual:uno.css";
+  import Nav from "$lib/Nav.svelte";
+  export const ssr = false;
+
+</script>
+
+<Nav />
+<div class="mt-15" >
+  <slot />
+</div>
+<style>
+  :global(body) {
+    background: radial-gradient(#27212e, #221530);
+    color: #d9c9ee;
+}</style>
+
